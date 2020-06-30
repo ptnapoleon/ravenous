dd9f5cd5a2f323fe6b5c12a4143f373adbf42c4d
Philip Thompson <ptnapoleon@gmail.com>
2020-06-30 12:30:00 -0400

    Log to console when searching

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -18,6 +18,7 @@ class SearchBar extends React.Component {
 
         this.handleTermChange = this.handleTermChange.bind(this);
         this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
     }
 
     getSortByClass(sortByOption) {
@@ -34,6 +35,11 @@ class SearchBar extends React.Component {
 
     handleLocationChange(event) {
         this.setState({location: event.target.value});
+    }
+
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        event.preventDefault();
     }
 
     renderSortByOptions() {
@@ -58,7 +64,7 @@ class SearchBar extends React.Component {
                     <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
                 <div className="SearchBar-submit">
-                    <a>Let's Go</a>
+                    <a onClick={this.handleSearch}>Let's Go</a>
                 </div>
             </div>
         );
